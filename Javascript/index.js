0bef10092bd3913e864d1296f055034033768ad1
Camilo Osorio Páez <142460465+caopdecode@users.noreply.github.com>
2024-12-07 11:47:34 -0500

    Data Analyst Portfolio - Version 1

---
+++ Javascript/index.js
@@ -0,0 +1,26 @@
+const pro1 = document.getElementById('segmentation');
+const about = document.getElementById('knowButton');
+const projectsSection = document.getElementById('projects');
+const header = document.querySelector('header');
+const brandName = document.querySelector('.brandName');
+const imgLogo = document.querySelector('.imgLogo');
+
+function toggleSticky() {
+  if (window.scrollY > 0) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+}
+
+window.addEventListener('scroll', toggleSticky);
+
+pro1.addEventListener('click', function() {
+    window.open("https://www.kaggle.com/code/caopdecode/customer-segmentation-project", "_blank")
+});
+
+about.addEventListener('click', function() { 
+    projectsSection.scrollIntoView({
+        behavior: 'smooth'
+      });
+});
